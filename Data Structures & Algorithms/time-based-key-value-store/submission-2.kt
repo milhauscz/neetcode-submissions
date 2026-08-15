d020d84f33785fdf370706dd66ccf07e3b5100ea
Miloš Černilovský <milhauscz@users.noreply.github.com>
2026-08-15 18:21:55 +0200

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.kt
@@ -0,0 +1,28 @@
+class TimeMap() {
+    val keyToTimestampAndValues = mutableMapOf<String,MutableList<Pair<Int,String>>>()
+
+    fun set(key: String, value: String, timestamp: Int) {
+        keyToTimestampAndValues.getOrPut(key) { mutableListOf<Pair<Int, String>>()}.add(timestamp to value)
+    }
+
+    fun get(key: String, timestamp: Int): String {
+        val timestampsToValues = keyToTimestampAndValues[key] ?: return ""
+        var l = 0
+        var r = timestampsToValues.size - 1
+        var res: Int? = null
+        while (l <= r) {
+            val mid = l + (r - l) / 2
+            if (timestampsToValues[mid].first == timestamp) {
+                res = mid
+                break
+            }
+            if (timestampsToValues[mid].first < timestamp) {
+                res = mid
+                l = mid + 1
+            } else {
+                r = mid - 1
+            }
+        }
+        return res?.let { timestampsToValues[it].second } ?: ""
+    }
+}
